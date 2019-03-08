34d11d7149bab881b7448c97510909142b62ea21
alainray <alainray@gmail.com>
2019-03-08 17:43:28 -0300

    Added video cutting features
    * Script will also now cut videos to the appropriate 900-1800s range.

--- ava_process.py
+++ ava_process.py
@@ -1,20 +1,20 @@
 # -*- coding: utf-8 -*-
 import requests
 import os
-import argparse
+import subprocess
 from tqdm import tqdm
-#Open file with train/test files
+import argparse
 # For each filename in file, download
 
 def download_file(filename, mode='trainval', base_dir=""):
   url = 'https://s3.amazonaws.com/ava-dataset/{}/{}'.format(mode, filename)
   file_path = "{}/{}".format(base_dir, filename)
   if check_file(filename, base_dir):
-    print("File {} already exists. Skipping.".format(filename))
+    print("\tFile {} already exists. Skipping.".format(filename))
   else:
     print("\tDownloading file {} from url: {}".format(filename, url))
     r = requests.get(url, allow_redirects=True)
-    print("Writing file {} to disk.".format(filename))
+    print("\tWriting file {} to disk.".format(filename))
     open(file_path, 'wb').write(r.content)
 
 def read_url_file(path):
@@ -25,25 +25,64 @@ def check_file(filename, base_dir):
   url = "{}/{}".format(base_dir, filename)
   return os.path.isfile(url)
 
+def process_video(filename, base_dir, output_dir):
+   # Construct command to trim the videos (ffmpeg required).
+    
+    start_time = 900
+    end_time = 1800
+    input_filename = "{}/{}".format(base_dir,filename)
+    output_filename = "{}/{}_900_1800.mp4".format(output_dir, filename)
+    status = False
 
-def parse_arguments():
-  description = 'Helper script for downloading AVA dataset videos'
-  p = argparse.ArgumentParser(description=description)
-  p.add_argument('input', type=str, help='CSV file containing filenames to download')
-  p.add_argument('-v', '--video_dir', type=str, help='Directory where videos will be stored', default='.')
-  p.add_argument('-m', '--mode', type=str, help='trainval or test', default="trainval")
+    if(not check_file(filename, base_dir)):
+      print("\tFile does not exist. Skipping:")
+    elif(check_file(output_filename, ".")):
+      print("\tVideo already processed. Skipping:")
+    else:
+      command = ['ffmpeg',
+                 '-i', '"%s"' % input_filename,
+                 '-ss', str(start_time),
+                 '-t', str(end_time - start_time),
+                 '-c:v', 'libx264', '-c:a', 'ac3', #ac3 needed for some videos
+                 '-threads', '1',
+                 '-loglevel', 'panic',
+                 '"{}"'.format(output_filename)]
+      command = ' '.join(command)
 
-  return p.parse_args()
+      try:
+          print("\tProcessing video: {}".format(filename))
+          output = subprocess.check_output(command, shell=True,
+                                           stderr=subprocess.STDOUT)
+      except subprocess.CalledProcessError as err:
+          print(status, err.output)
+          return status, err.output
 
-#Just parsing arguments 
-args = parse_arguments()
+      # Check if the video was successfully saved.
+      status = os.path.exists(output_filename)
+    return status
 
+#Parser time
 
-input_filename = args.input
-video_dir = args.video_dir
+parser = argparse.ArgumentParser()
+parser.add_argument( "input" , help="Filename of file with video filenames")
+parser.add_argument("--video_dir", "-v", help="Directory to store downloaded videos", default="ava_dataset")
+parser.add_argument("--output_dir", "-o", help="Directory to store processed videos", default="processed")
+parser.add_argument("--mode", "-m", help="Whether it's trainval or test mode", default="trainval")
+parser.add_argument("-f", "--function", help="Whether you want to download, cut videos or do both. Values are: 'd', 'c', 'dc'", default='d')
+
+args = parser.parse_args()
+#Open file with train/test files
+url_file = args.input
+
+base_dir = args.video_dir
+output_dir = args.output_dir
 mode = args.mode
+function = args.function
+videos = read_url_file(url_file)
 
-videos = read_url_file(input_filename)
-
-for filename in tqdm(videos):
-  download_file(filename, mode, video_dir)
+# Download and/or process each video in file
+for i,filename in enumerate(tqdm(videos)):
+  if function.find('d')>=0:
+    download_file(filename, mode, base_dir)
+  if function.find('c')>=0:  
+    process_video(filename,base_dir,output_dir)
